fixed calculation of density matrix for model 1 (noisy latent)

hyperparameter_tuning/noisy_latent_accuracy_table.py
@@ -10,8 +10,8 @@ import pandas as pd
 from pandas.plotting import table
 
 q = 0.4
-p1_vals = np.arange(0.1, 1.0, 0.1).round(decimals=1)
-p2_vals = np.arange(0.1, 1.0, 0.1).round(decimals=1)
+p1_vals = np.append(np.insert(np.arange(0.1, 1.0, 0.1).round(decimals=1), 0, 0.01), 0.99)
+p2_vals = np.append(np.insert(np.arange(0.1, 1.0, 0.1).round(decimals=1), 0, 0.01), 0.99)
 
 accurate_result = "latent Markovizing witness"
 
@@ -22,7 +22,7 @@ def get_xyz(q, p1, p2):
             for z in range(0, 4):
                 prod = 1
                 prod *= q if (z & 2) == 2 else (1-q)
-                prod *= q if (z & 1) == 2 else (1-q)
+                prod *= q if (z & 1) == 1 else (1-q)
                 prod *= p1 if ((z&2) != (x&2)) else (1-p1)
                 prod *= p1 if ((z&1) != (x&1)) else (1-p1)
                 prod *= p2 if ((z&2) != (y&2)) else (1-p2)
@@ -40,7 +40,7 @@ dz = 4
 
 penalties = ih.penalties
 tolerance = ih.tolerance
-entrop_thresh = ih.entrop_thresh
+entrop_thresh = 0.9#ih.entrop_thresh
 extern_thresh = ih.extern_thresh
 dep_gate = ih.dep_gate
 smoothing = ih.smoothing
@@ -59,14 +59,22 @@ for i in range(len(p1_vals)):
     results.append([])
     for j in range(len(p2_vals)):
         problem = qci.QProblem(qci.tr_z(get_xyz(q, p1_vals[i], p2_vals[j]), dx, dy, dz), dx, dy, dz)
-        res = qci.QInferGraph(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
-                         smoothing, damping, log_reg, n, null_fam, sig_lvl).result_message
-        if accurate_result == res[:len(accurate_result)]:
-            results[i].append('T')
-            print(f"{p1_vals[i]}, {p2_vals[j]}: T")
+        # res = qci.QInferGraph(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
+        #                  smoothing, damping, log_reg, n, null_fam, sig_lvl).result_message
+        # if accurate_result == res[:len(accurate_result)]:
+        #     results[i].append('T')
+        #     print(f"{p1_vals[i]}, {p2_vals[j]}: T")
+        # else:
+        #     results[i].append('F')
+        #     print(f"{p1_vals[i]}, {p2_vals[j]}: F")
+        alpha = qci.getMinAlpha(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
+                                smoothing, damping, log_reg, n, null_fam, sig_lvl)
+        if alpha is not None:
+            results[i].append((np.ceil(alpha*100)/100))
+            print(f"{p1_vals[i]}, {p2_vals[j]}: {(np.ceil(alpha*100)/100)}")
         else:
-            results[i].append('F')
-            print(f"{p1_vals[i]}, {p2_vals[j]}: F")
+            results[i].append(None)
+            print(f"{p1_vals[i]}, {p2_vals[j]}: {None}")
 
 
 #Make table

lib/quantum_causal_inference.py
@@ -702,10 +702,27 @@ def QInferGraph(problem: QProblem, penalties: list[float], tolerance: float, ent
     if (common_entropy is not None) and (common_entropy[3] <= extern_thresh) and ((quantile is None) or (common_entropy[3] < quantile)):
         result_message =  f"latent Markovizing witness\n\npenalty: {common_entropy[0]}\nmi_xy|z: {common_entropy[2]}\ns_z: {common_entropy[3]}"
     else:
-        result_message =  f"not latent (common entropy above threshold){f"\n\npenalty: {common_entropy[0]}\nmi_xy|z: {common_entropy[2]}\ns_z: {common_entropy[3]}" if common_entropy is not None else ""}"
+        result_message =  f"not latent (common entropy above threshold)"
+        result_message += f"\npenalty: {common_entropy[0]}\nmi_xy|z: {common_entropy[2]}\ns_z: {common_entropy[3]}" if common_entropy is not None else ""
+        result_message += f"\n% Markovizing: {len(candidates)/n}\nmi_xy|z: {common_entropy[2]}\ns_z: {common_entropy[3]}" if common_entropy is not None else ""
     
     return QGraphResult(result_message, candidates)
     
+def getMinAlpha(problem: QProblem, penalties: list[float], tolerance: float, entrop_thresh: float, extern_thresh: float, dep_gate: float,
+                 smoothing: float, damping: float, log_reg: float, n: int, null_fam: list[QProblem], sig_lvl: float):
+    result = QInferGraph(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, smoothing, damping, log_reg, n, null_fam, sig_lvl)
+    
+    esti_state = problem.esti_state
+    dx = problem.dx
+    dy = problem.dy
 
+    p_x = np.trace(esti_state.reshape(dx, dy, dx, dy), axis1=1, axis2=3)
+    p_y = np.trace(esti_state.reshape(dx, dy, dx, dy), axis1=0, axis2=2)
+    entrop = min(vn_entropy(p_x), vn_entropy(p_y))
+
+    if result.candidate_entropies is None or len(result.candidate_entropies) < 1:
+        return None
+    else:
+        return result.candidate_entropies[0][3] / entrop
     
     

open_test.py
@@ -1,19 +1,21 @@
 import numpy as np
+import numpy.linalg as lin
 import lib.quantum_causal_inference as qci
+import lib.inference_hyperparams as ih
 import matplotlib.pyplot as plt
 
 A = np.zeros((64, 64))
 
 q = 0.4
-p1 = 0.1
-p2 = 0.2
+p1 = 0.3
+p2 = 0.4
 
 for x in range(0, 4):
     for y in range(0, 4):
         for z in range(0, 4):
             prod = 1
             prod *= q if (z & 2) == 2 else (1-q)
-            prod *= q if (z & 1) == 2 else (1-q)
+            prod *= q if (z & 1) == 1 else (1-q)
             prod *= p1 if ((z&2) != (x&2)) else (1-p1)
             prod *= p1 if ((z&1) != (x&1)) else (1-p1)
             prod *= p2 if ((z&2) != (y&2)) else (1-p2)
@@ -21,34 +23,43 @@ for x in range(0, 4):
             A[x*16 + y*4 + z][x*16 + y*4 + z] = prod
 
 print(A)
-
+print(np.trace(A))
 prob = qci.tr_z(A, 4, 4, 4)
-print(prob)
+print(np.trace(prob))
 
 dx = 4
 dy = 4
 dz = 4
 
+p_x = np.trace(prob.reshape(dx, dy, dx, dy), axis1=1, axis2=3)
+p_y = np.trace(prob.reshape(dx, dy, dx, dy), axis1=0, axis2=2)
+
+print(qci.mi_xy(prob, dx, dy))
+
 problem = qci.QProblem(prob, dx, dy, dz)
 
 # penalties = [0, 0.1, 0.5, 0.8, 1, 1.5, 2, 3]
-penalties = np.arange(0, 100) / 100
-tolerance = 0.05
-entrop_thresh = 0.8
-extern_thresh = None
-dep_gate = 0
-smoothing = 0.01
-damping = 0.2
-log_reg = 0.2
-n = 1000
+penalties = ih.penalties
+tolerance = ih.tolerance
+entrop_thresh = ih.entrop_thresh
+extern_thresh = ih.extern_thresh
+dep_gate = 0.001#ih.dep_gate
+smoothing = ih.smoothing
+damping = ih.damping
+log_reg = ih.log_reg
+n = 100
 
 null_fam = []#[qci.QProblem(esti_state3, dx, dy, dz), qci.QProblem(esti_state1, dx, dy, dz)]
-sig_lvl = 0.2
+sig_lvl = ih.sig_lvl
 
 result = qci.QInferGraph(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
                          smoothing, damping, log_reg, n, null_fam, sig_lvl)
 
 print(result.result_message)
+
+print(qci.tr_xy(result.candidate_entropies[0][1], dx, dy, dz))
+print(lin.eig(qci.tr_xy(result.candidate_entropies[0][1], dx, dy, dz))[0])
+print(np.trace(result.candidate_entropies[0][1]))
 
 if result.candidate_entropies is not None and len(result.candidate_entropies) > 0:
     entropies_list = []