69ab1532abdc4c1960b62cb51b45c2c523ac1f99
grm228 <grm228@lehigh.edu>
2026-08-06 23:15:42 -0400

    added unitary invariance checks

---
+++ null_calibration/unitary_invariance/dephased_ghz.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.insert(1, 'C:\\Users\\gevmo\\OneDrive\\Music\\Documents\\2026 - Summer\\Causal Inference\\quantum-latent-cause')
+
+import lib.quantum_causal_inference as qci
+import lib.inference_hyperparams as ih
+import numpy as np
+import math
+
+dx = 2
+dy = 2
+dz = 2
+
+esti_state = np.array([[0.5, 0, 0, 0], 
+                       [0, 0, 0, 0], 
+                       [0, 0, 0, 0], 
+                       [0, 0, 0, 0.5]])
+
+ux = np.array([[1, 1], [1, -1]])/math.sqrt(2)
+uy = np.array([[0, complex(0, -1)], [complex(0, 1), 0]])
+
+operator = np.kron(ux, uy)
+
+problem = qci.QProblem(esti_state, dx, dy, dz)
+problem_alt = qci.QProblem(np.matmul(operator, np.matmul(esti_state, np.conjugate(np.transpose(operator)))), dx, dy, dz)
+
+penalties = ih.penalties
+tolerance = ih.tolerance
+entrop_thresh = ih.entrop_thresh
+extern_thresh = ih.extern_thresh
+dep_gate = ih.dep_gate
+smoothing = ih.smoothing
+damping = ih.damping_ghz
+log_reg = ih.log_reg_ghz
+n = ih.n
+
+null_fam = []
+sig_lvl = ih.sig_lvl
+
+result = qci.QInferGraph(problem, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
+                         smoothing, damping, log_reg, n, null_fam, sig_lvl, True)
+
+print(result.result_message)
+
+result_alt = qci.QInferGraph(problem_alt, penalties, tolerance, entrop_thresh, extern_thresh, dep_gate, 
+                         smoothing, damping, log_reg, n, null_fam, sig_lvl, True)
+
+print(result_alt.result_message)
